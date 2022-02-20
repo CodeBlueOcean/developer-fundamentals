93cc4496350043638f1f4985f6548ed8d2cac9e1
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-02-19 23:28:45 -0600

    Add files via upload

---
+++ developer-fundamentals.py
@@ -0,0 +1,16 @@
+# assign name variable a value of Andrei string
+name = 'Andrei' # this assigns to a variable
+is_cool = False
+
+is_cool = True
+print(bool('True'))
+
+
+a = 'Andrei'
+is_cool = False # is cool flag
+
+is_cool = True
+
+print(bool('True'))
+
+#adding comments when necessary 
---
+++ developer-fundamentals3.py
@@ -0,0 +1,14 @@
+# Knowing when to you a dictionary vs a list in data structure is important
+# dictionary can holds a lot of variables while list has order, dictionary doesn't have order
+dictionary = {
+    'weapons' : [1,2,3], 
+    'greeting': 'hello', 
+    'is_Magic' : True
+}
+
+my_list = ['lslsjl'
+
+]
+print(my_list[0]['a'][2])
+# above code grab the first {}, 'a', index 2 
+print(dictionary['a'][1])
